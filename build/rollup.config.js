8c05d31661bbad708ee2f599179b9eb2972864c5
GitHub <action@github.com>
2022-11-02 11:54:54 +0000

    Fixing config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -12,198 +12,200 @@ import minimist from 'minimist';
 
 // Get browserslist config and remove ie from es build targets
 const esbrowserslist = fs.readFileSync('./.browserslistrc')
-  .toString()
-  .split('\n')
-  .filter((entry) => entry && entry.substring(0, 2) !== 'ie');
+    .toString()
+    .split('\n')
+    .filter((entry) => entry && entry.substring(0, 2) !== 'ie');
 
 const argv = minimist(process.argv.slice(2));
 
 const projectRoot = path.resolve(__dirname, '..');
 
 const baseConfig = {
-  input: 'public/entry.ts',
+    input: 'public/entry.ts',
 };
 
 const basePlugins = {
-  forAll: [
-    alias({
-      resolve: ['.js', '.ts'],
-      entries: {
-        '@': path.resolve(projectRoot, 'public'),
-      },
-    }),
-    eslint(),
-  ],
-  replace: {
-    preventAssignment: true,
-    'process.env.NODE_ENV': JSON.stringify('production'),
-    'process.env.ES_BUILD': JSON.stringify('false'),
-  },
+    forAll: [
+        alias({
+            resolve: ['.js', '.ts'],
+            entries: {
+                '@': path.resolve(projectRoot, 'public'),
+            },
+        }),
+        eslint(),
+    ],
+    replace: {
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        'process.env.ES_BUILD': JSON.stringify('false'),
+    },
 };
 
 const babelConfig = {
-  babelHelpers: 'bundled',
-  exclude: 'node_modules/**',
-  extensions: ['.js', '.ts',],
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+    extensions: ['.js', '.ts',],
 };
 
 // ESM/UMD/IIFE shared settings: externals
 // Refer to https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
 const external = [
-  // list external dependencies, exactly the way it is written in the import statement.
-  // eg. 'jquery'
-  'ajv',
-  'date-fns',
-  'jsona',
-  'jsona/lib/simplePropertyMappers',
-  'lodash/capitalize',
-  'lodash/clone',
-  'lodash/get',
-  'lodash/uniq',
-  'uuid',
-  'vuex',
-  '@fastybird/metadata-library',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.condition.json',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.trigger.json',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.notification.json',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.action.json',
-  '@vuex-orm/core',
+    // list external dependencies, exactly the way it is written in the import statement.
+    // e.g. 'jquery'
+    'ajv',
+    'jsona',
+    'jsona/lib/simplePropertyMappers',
+    'lodash/capitalize',
+    'lodash/clone',
+    'lodash/get',
+    'lodash/uniq',
+    'uuid',
+    'vue',
+    'vuex',
+    '@fastybird/metadata-library',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.condition.json',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.trigger.json',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.notification.json',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.action.json',
+    '@vuex-orm/core',
+    'date-fns',
 ];
 
 // UMD/IIFE shared settings: output.globals
 // Refer to https://rollupjs.org/guide/en#output-globals for details
 const globals = {
-  // Provide global variable names to replace your external imports
-  // eg. jquery: '$'
-  ajv: 'Ajv',
-  'date-fns': 'DateFns',
-  jsona: 'Jsona',
-  'jsona/lib/simplePropertyMappers': 'defineRelationGetter',
-  'lodash/capitalize': 'capitalize',
-  'lodash/clone': 'clone',
-  'lodash/get': 'get',
-  'lodash/uniq': 'uniq',
-  uuid: 'v4',
-  vuex: 'Vuex',
-  '@fastybird/metadata-library': 'Metadata',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.condition.json': 'ConditionExchangeEntitySchema',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.trigger.json': 'TriggerExchangeEntitySchema',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.notification.json': 'NotificationExchangeEntitySchema',
-  '@fastybird/metadata-library/resources/schemas/triggers-module/entity.action.json': 'ActionExchangeEntitySchema',
-  '@vuex-orm/core': 'OrmCore',
+    // Provide global variable names to replace your external imports
+    // e.g. jquery: '$'
+    ajv: 'Ajv',
+    jsona: 'Jsona',
+    'jsona/lib/simplePropertyMappers': 'defineRelationGetter',
+    'lodash/capitalize': 'capitalize',
+    'lodash/clone': 'clone',
+    'lodash/get': 'get',
+    'lodash/uniq': 'uniq',
+    uuid: 'v4',
+    vue: 'Vue',
+    vuex: 'Vuex',
+    '@fastybird/metadata-library': 'ModulesMetadata',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.condition.json': 'ConditionExchangeEntitySchema',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.trigger.json': 'TriggerExchangeEntitySchema',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.notification.json': 'NotificationExchangeEntitySchema',
+    '@fastybird/metadata-library/resources/schemas/triggers-module/entity.action.json': 'ActionExchangeEntitySchema',
+    '@vuex-orm/core': 'OrmCore',
+    'date-fns': 'dateFns',
 };
 
 // Customize configs for individual targets
 const buildFormats = [];
 
 if (!argv.format || argv.format === 'es') {
-  const esConfig = {
-    ...baseConfig,
-    external,
-    output: {
-      file: 'dist/triggers-module.esm.js',
-      format: 'esm',
-      exports: 'named',
-    },
-    plugins: [
-      replace({
-        ...basePlugins.replace,
-        'process.env.ES_BUILD': JSON.stringify('true'),
-      }),
-      ...basePlugins.forAll,
-      babel({
-        ...babelConfig,
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              targets: esbrowserslist,
-            },
-          ],
+    const esConfig = {
+        ...baseConfig,
+        external,
+        output: {
+            file: 'dist/triggers-module.esm.js',
+            format: 'esm',
+            exports: 'named',
+        },
+        plugins: [
+            replace({
+                ...basePlugins.replace,
+                'process.env.ES_BUILD': JSON.stringify('true'),
+            }),
+            ...basePlugins.forAll,
+            babel({
+                ...babelConfig,
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            targets: esbrowserslist,
+                        },
+                    ],
+                ],
+            }),
+            commonjs(),
         ],
-      }),
-      commonjs(),
-    ],
-  };
-  buildFormats.push(esConfig);
+    };
+    buildFormats.push(esConfig);
 }
 
 if (!argv.format || argv.format === 'cjs') {
-  const umdConfig = {
-    ...baseConfig,
-    external,
-    output: {
-      compact: true,
-      file: 'dist/triggers-module.ssr.js',
-      format: 'cjs',
-      name: 'TriggersModule',
-      exports: 'named',
-      globals,
-    },
-    plugins: [
-      replace(basePlugins.replace),
-      ...basePlugins.forAll,
-      babel(babelConfig),
-      commonjs(),
-    ],
-  };
-  buildFormats.push(umdConfig);
+    const umdConfig = {
+        ...baseConfig,
+        external,
+        output: {
+            compact: true,
+            file: 'dist/triggers-module.ssr.js',
+            format: 'cjs',
+            name: 'Triggers',
+            exports: 'named',
+            globals,
+        },
+        plugins: [
+            replace(basePlugins.replace),
+            ...basePlugins.forAll,
+            babel(babelConfig),
+            commonjs(),
+        ],
+    };
+    buildFormats.push(umdConfig);
 }
 
 if (!argv.format || argv.format === 'iife') {
-  const unpkgConfig = {
-    ...baseConfig,
-    external,
-    output: {
-      compact: true,
-      file: 'dist/triggers-module.min.js',
-      format: 'iife',
-      name: 'TriggersModule',
-      exports: 'named',
-      globals,
-    },
-    plugins: [
-      replace(basePlugins.replace),
-      ...basePlugins.forAll,
-      babel(babelConfig),
-      commonjs(),
-      terser({
+    const unpkgConfig = {
+        ...baseConfig,
+        external,
         output: {
-          ecma: 5,
+            compact: true,
+            file: 'dist/triggers-module.min.js',
+            format: 'iife',
+            name: 'Triggers',
+            exports: 'named',
+            globals,
         },
-      }),
-    ],
-  };
-  buildFormats.push(unpkgConfig);
+        plugins: [
+            replace(basePlugins.replace),
+            ...basePlugins.forAll,
+            babel(babelConfig),
+            commonjs(),
+            terser({
+                output: {
+                    ecma: 5,
+                },
+            }),
+        ],
+    };
+    buildFormats.push(unpkgConfig);
 }
 
 buildFormats.push({
-  ...baseConfig,
-  external,
-  output: {
-    file: 'dist/triggers-module.d.ts',
-    format: 'es',
-  },
-  plugins: [
-    replace({
-      ...basePlugins.replace,
-      'process.env.ES_BUILD': JSON.stringify('true'),
-    }),
-    ...basePlugins.forAll,
-    babel({
-      ...babelConfig,
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: esbrowserslist,
-          },
-        ],
-      ],
-    }),
-    commonjs(),
-    dts(),
-  ],
+    ...baseConfig,
+    external,
+    output: {
+        file: 'dist/triggers-module.d.ts',
+        format: 'es',
+    },
+    plugins: [
+        replace({
+            ...basePlugins.replace,
+            'process.env.ES_BUILD': JSON.stringify('true'),
+        }),
+        ...basePlugins.forAll,
+        babel({
+            ...babelConfig,
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: esbrowserslist,
+                    },
+                ],
+            ],
+        }),
+        commonjs(),
+        dts(),
+    ],
 });
 
 // Export config
