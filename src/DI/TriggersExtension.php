74f4c27f539ba0ac739123fb46045313f0aaa568
GitHub <action@github.com>
2023-10-13 08:39:38 +0000

    [sonoff-connector] Refactoring to new structure (#148)

--- src/DI/TriggersExtension.php
+++ src/DI/TriggersExtension.php
@@ -58,7 +58,6 @@ class TriggersExtension extends DI\CompilerExtension
 		string $extensionName = self::NAME,
 	): void
 	{
-		// @phpstan-ignore-next-line
 		$config->onCompile[] = static function (
 			BootstrapBoot\Configurator $config,
 			DI\Compiler $compiler,
